bf0b9e2f4dbc0c58c62d9cd8895e7be9bc3fc82a
Tim Olson <tim.olson51@gmail.com>
2021-10-06 20:54:28 -0500

    basic bad templating

--- i3/install.sh
+++ i3/install.sh
@@ -9,7 +9,6 @@ packages=(
     # 'wired' # ope city
     'playerctl'
     'ttf-font-awesome'
-    'xorg-xrandr'
 )
 
 not_installed=()
@@ -28,14 +27,53 @@ fi
 
 CONFIG_DIR=$HOME/.config
 
-mkdir -p $CONFIG_DIR/{i3,i3status-rust}
+# Basic templating. I kind of hate this, is there a jinja/askama cli?...
 
-cp -r i3/ $CONFIG_DIR
-cp -r i3status-rust/ $CONFIG_DIR
+###
+# Xresources
+### 
+HARDWARE=$(cat $CONFIG_DIR/tolson-hardware)
+if [[ "$HARDWARE" -eq "desktop" ]]; then
+    sed '1s/^/Xft.dpi: 98\n/' Xresources >> Xresources-tmpled
+elif [[ "$HARDWARE" -eq "x1yoga" ]]; then
+    sed '1s/^/Xft.dpi: 98\n/' Xresources >> Xresources-tmpled
+elif [[ "$HARDWARE" -eq "p5540" ]]; then
+    sed '1s/^/Xft.dpi: ???\n/' Xresources >> Xresources-tmpled
+fi 
+
+###
+# i3status-rust
+### 
+cp i3status-rust/config.toml i3status-rust/config.toml-tmpled
+if [[ "$HARDWARE" -eq "desktop" ]]; then
+    cat >> i3status-rust/config.toml-tmpled <<EOF
+[[block]]
+block = "time"
+interval = 5
+format = "%a %d/%m %R"
+EOF
+else
+    cat >> i3status-rust/config.toml-tmpled <<EOF
+[[block]]
+block = "battery"
+interval = 10
+format = " {percentage}"
+
+[[block]]
+block = "time"
+interval = 5
+format = "%a %d/%m %R"
+EOF
+fi
+
+mkdir -p $CONFIG_DIR/{i3,i3status-rust}
+cp i3/config $CONFIG_DIR/i3/config
+cp i3status-rust/config.toml-tmpled $CONFIG_DIR/i3status-rust/config.toml
 
 cp lock.sh $HOME/tin
-cp Xresources $HOME/.Xresources
+cp Xresources-tmpled $HOME/.Xresources
 
 xrdb ~/.Xresources 
 i3-msg reload
 i3-msg restart
+
